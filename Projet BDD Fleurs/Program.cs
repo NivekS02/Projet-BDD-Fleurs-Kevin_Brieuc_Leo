2bc540951fddd61f999dea00f3f596dac828525a
NivekS02 <kevin.kui@edu.devinci.fr>
2023-04-23 12:43:03 +0200

    modif 5

--- Projet BDD Fleurs/Program.cs
+++ Projet BDD Fleurs/Program.cs
@@ -10,9 +10,9 @@ namespace Projet_BDD_Fleurs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Leo et Bieuc, quels cracks"); 
+            Console.WriteLine("Leo et Bieuc, quels cracks");
             // ceci est un test fait par Brieuc attention ne tentez pas de le reproduire chez vous
-            
+            // allleeeeez let's goooo c'est les vacances, on va taffer XD
         }
     }
 }
